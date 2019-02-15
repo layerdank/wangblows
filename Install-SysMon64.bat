da676098624ef72a5f84c2f2e6ff6915dd19c940
Zinaut <47339616+Zinaut@users.noreply.github.com>
2019-02-14 23:40:29 -0800

    Add files via upload

---
+++ Install-SysMon64.bat
@@ -0,0 +1,3 @@
+﻿
+bitsadmin /transfer myDownloadJob /download /priority FOREGROUND "https://live.sysinternals.com/SysMon64.exe" "C:\Users\Public\Downloads\SysMon64.exe"
+"C:\Users\Public\Downloads\SysMon64.exe" -accepteula -i -l -n -h MD5
