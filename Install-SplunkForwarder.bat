c4a4a6f570d7c4fe02c836bc17406bf6bc0f1bcd
Zinaut <47339616+Zinaut@users.noreply.github.com>
2019-02-15 21:32:59 -0800

    Update Install-SplunkForwarder.bat
    Changed the script so it prompts for an IP address of Splunk.

--- Install-SplunkForwarder.bat
+++ Install-SplunkForwarder.bat
@@ -1,4 +1,5 @@
-﻿
+set /p splunk_ip="Enter Splunk IP Address: "
+
 bitsadmin /transfer myDownloadJob /download /priority FOREGROUND "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=7.2.4&product=universalforwarder&filename=splunkforwarder-7.2.4-8a94541dcfac-x64-release.msi&wget=true" "C:\Users\Public\Downloads\splunkforwarder-7.2.4-8a94541dcfac-x64-release.msi"
 
-msiexec.exe /i "C:\Users\Public\Downloads\splunkforwarder-7.2.4-8a94541dcfac-x64-release.msi" RECEIVING_INDEXER="splunk.layer8.edu:9997" DEPLOYMENT_SERVER="splunk.layer8.edu:8089" LAUNCHSPLUNK=1 SERVICESTARTTYPE=auto AGREETOLICENSE=yes /quiet
+msiexec.exe /i "C:\Users\Public\Downloads\splunkforwarder-7.2.4-8a94541dcfac-x64-release.msi" RECEIVING_INDEXER="%splunk_ip%:9997" DEPLOYMENT_SERVER="%splunk_ip%:8089" LAUNCHSPLUNK=1 SERVICESTARTTYPE=auto AGREETOLICENSE=yes /quiet
